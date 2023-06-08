8ee9f93f798e981b4f0027904cf04b4725619875
terisuke <terisuke1115@gmail.com>
2023-06-08 22:08:00 +0900

    create file

--- README.md
+++ README.md
@@ -1,10 +1,26 @@
-# streamlit_face_emotion
-streamlit上でpy-featによる表情分析を行います。入力は画像、カメラどちらでも可能です。
-Docker用
+# ここにタイトルを入れる
+Face emotion app
+## DEMO
 
----
-### py-feat Licence
+  - デプロイしている場合はURLを記入（任意）
 
-MIT License
-Copyright (c) 2022, Jin Hyun Cheong, Tiankang Xie, Sophie Byrne, Eshin Jolly, Luke Chang
-https://github.com/cosanlab/py-feat/blob/main/LICENSE
+## 紹介と使い方
+
+  - streamlitアプリ！
+
+  - 画像と内蔵カメラで撮った写真から表情を分析できるよ！
+
+## 工夫した点
+
+  - いかにシンプルなコードで表情分析を作れるかを調べて研究しました
+
+## 苦戦した点
+
+  - pythonもstreamlitもDockerも初めてで開発環境作りめっちゃがんめっちゃ頑張った！
+    これからも定期的に触っていこうね！
+
+## 参考にした web サイトなど
+
+  - https://yolo-kiyoshi.com/2021/09/22/post-2760/
+  - https://zenn.dev/littledarwin/articles/34fc64822f2e9d
+  - https://py-feat.org/pages/intro.html
--- src/app.py
+++ src/app.py
@@ -1,16 +1,8 @@
-#ライブラリ
+# ライブラリ
 import streamlit as st
-import pandas as pd
 import numpy as np
 import cv2
-import numpy as np
-import pandas as pd
-import re
-import os
-import matplotlib.pyplot as plt
-from sklearn import preprocessing
 from PIL import Image
-
 
 
 from feat import Detector
@@ -25,44 +17,29 @@ detector = Detector(
     au_model=au_model,
     emotion_model=emotion_model
 )
-from feat.utils import get_test_data_path
 
 
-
-
-
-#本編  
+# 本編
 st.title("Face emotion app")
 
 img_source = st.radio("画像のソースを選択してください。",
-                              ("画像をアップロード", "カメラで撮影"))
+                      ("画像をアップロード", "カメラで撮影"))
 if img_source == "カメラで撮影":
-  img_file_buffer = st.camera_input("カメラで撮影")
+    img_file_buffer = st.camera_input("カメラで撮影")
 elif img_source == "画像をアップロード":
-  img_file_buffer = st.file_uploader("ファイルを選択")
+    img_file_buffer = st.file_uploader("ファイルを選択")
 else:
     pass
 
 
+if img_file_buffer:
+    img_file_buffer_2 = Image.open(img_file_buffer)
+    img_file = np.array(img_file_buffer_2)
+    cv2.imwrite('temporary.jpg', img_file)
+    image_prediction = detector.detect_image("temporary.jpg")
+    image_prediction = image_prediction[[
+        "anger", "disgust", "fear", "happiness", "sadness", "surprise", "neutral"]]
+    emotion = image_prediction.idxmax(axis=1)[0]
 
-
-  
-if img_file_buffer :
-  img_file_buffer_2 = Image.open(img_file_buffer)
-  img_file = np.array(img_file_buffer_2)
-  cv2.imwrite('temporary.jpg', img_file)
-  image_prediction = detector.detect_image("temporary.jpg")
-  image_prediction = image_prediction[["anger", "disgust", "fear", "happiness", "sadness", "surprise", "neutral"]]
-  emotion = image_prediction.idxmax(axis = 1)[0]
-
-  st.markdown("#### あなたの表情は")
-  st.markdown("### {}です".format(emotion))
-  
-  
-  
-  
-
-
-
-
-
+    st.markdown("#### あなたの表情は")
+    st.markdown("### {}です".format(emotion))
